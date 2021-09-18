5dcda42e7320af3137a2627aefc264b09ecff865
trashgod <trashgod@gmail.com>
2021-09-17 20:01:19 -0400

    Control origin; use bindings.

--- src/main/java/org/example/math/Controls.java
+++ src/main/java/org/example/math/Controls.java
@@ -1,11 +1,12 @@
 package org.example.math;
 
-import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.ToolBar;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
@@ -22,26 +23,28 @@ public class Controls {
     public Pane createValuesPane() {
         Spinner<Number> mSpinner = new Spinner<>(MIN, MAX, Graph.MULTIPLIER, 0.1);
         mSpinner.setEditable(true);
-        graph.getM().bindBidirectional(mSpinner.getValueFactory().valueProperty());
+        graph.mProperty().bindBidirectional(mSpinner.getValueFactory().valueProperty());
         Spinner<Number> pSpinner = new Spinner<>(MIN, MAX, Graph.POINTS);
         pSpinner.setEditable(true);
-        graph.getP().bindBidirectional(pSpinner.getValueFactory().valueProperty());
+        graph.pProperty().bindBidirectional(pSpinner.getValueFactory().valueProperty());
         return new Pane(new ToolBar(
             new Label("Multiplier:"), mSpinner,
             new Label("Points:"), pSpinner));
     }
 
     public Pane createSettingsPane() {
-        ColorPicker bgPicker = new ColorPicker(graph.getB().get());
-        bgPicker.setOnAction((ActionEvent e) -> {
-            graph.getB().set(bgPicker.getValue());
-        });
-        ColorPicker fgPicker = new ColorPicker(graph.getC().get());
-        fgPicker.setOnAction((ActionEvent e) -> {
-            graph.getC().set(fgPicker.getValue());
-        });
-        final VBox vBox = new VBox(bgPicker, fgPicker);
+        ColorPicker bgPicker = new ColorPicker(graph.bProperty().get());
+        bgPicker.setTooltip(new Tooltip("Background color."));
+        graph.bProperty().bind(bgPicker.valueProperty());
+        ColorPicker fgPicker = new ColorPicker(graph.cProperty().get());
+        fgPicker.setTooltip(new Tooltip("Foreground color."));
+        graph.cProperty().bind(fgPicker.valueProperty());
+        CheckBox cb = new CheckBox("Flip origin:");
+        cb.setSelected(graph.fProperty().get());
+        graph.fProperty().bind(cb.selectedProperty());
+        final VBox vBox = new VBox(bgPicker, fgPicker, cb);
         vBox.setAlignment(Pos.CENTER);
+        vBox.setSpacing(8);
         return vBox;
     }
 }
--- src/main/java/org/example/math/Graph.java
+++ src/main/java/org/example/math/Graph.java
@@ -1,9 +1,11 @@
 package org.example.math;
 
 import javafx.beans.InvalidationListener;
+import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
@@ -13,6 +15,9 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.ArcType;
 
+/**
+ * A Graph rendering chords connecting points on a circle.
+ */
 public class Graph {
 
     public static final int POINTS = 128;
@@ -26,6 +31,7 @@ public class Graph {
     private final DoubleProperty m = new SimpleDoubleProperty(MULTIPLIER);
     private final ObjectProperty<Color> b = new SimpleObjectProperty<>(Color.WHITE);
     private final ObjectProperty<Color> c = new SimpleObjectProperty<>(Color.BLUE);
+    private final BooleanProperty f = new SimpleBooleanProperty(true);
     private final InvalidationListener listener = ((o) -> update());
 
     public Graph() {
@@ -37,6 +43,7 @@ public class Graph {
         pane.heightProperty().addListener(listener);
         p.addListener(listener);
         m.addListener(listener);
+        f.addListener(listener);
         b.addListener(listener);
         c.addListener(listener);
         update();
@@ -55,12 +62,13 @@ public class Graph {
         g.fillRect(0, 0, w, h);
         g.setStroke(c.get());
         g.strokeArc(cx - r, cy - r, 2 * r, 2 * r, 0, 359, ArcType.CHORD);
+        double offset = (f.get() ? Math.PI : 0);
         for (int i = 0; i < p.get(); i++) {
-            double t1 = 2 * Math.PI * i / p.get() + Math.PI;
+            double t1 = 2 * Math.PI * i / p.get() + offset;
             double x1 = Math.cos(t1) * r + cx;
             double y1 = Math.sin(t1) * r + cy;
             double j = (i * m.get()) % ((double) p.get());
-            double t2 = 2 * Math.PI * j / p.get() + Math.PI;
+            double t2 = 2 * Math.PI * j / p.get() + offset;
             double x2 = Math.cos(t2) * r + cx;
             double y2 = Math.sin(t2) * r + cy;
             g.strokeLine(x1, y1, x2, y2);
@@ -70,28 +78,35 @@ public class Graph {
     /**
      * @return this graph's number of points property
      */
-    public IntegerProperty getP() {
+    public IntegerProperty pProperty() {
         return p;
     }
 
     /**
      * @return this graph's multiplier property
      */
-    public DoubleProperty getM() {
+    public DoubleProperty mProperty() {
         return m;
     }
 
     /**
      * @return this graph's background color property
      */
-    public ObjectProperty<Color> getB() {
+    public ObjectProperty<Color> bProperty() {
         return b;
+    }
+
+    /**
+     * @return this graph's flipped property
+     */
+    public BooleanProperty fProperty() {
+        return f;
     }
 
     /**
      * @return this graph's foreground color property
      */
-    public ObjectProperty<Color> getC() {
+    public ObjectProperty<Color> cProperty() {
         return c;
     }
 
