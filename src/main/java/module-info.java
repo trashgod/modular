19e11a44f0b5be77d6e12effca78da8ddade7c05
trashgod <trashgod@gmail.com>
2022-12-14 18:25:09 -0500

    Update module-info.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,7 @@ module modular {
     requires javafx.controls;
     requires javafx.fxml;
     exports org.example;
+    exports org.example.gui;
+    exports org.example.math;
     opens org.example to javafx.fxml;
 }
